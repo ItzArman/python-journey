985119f87a8ed3a4d2592c06fcc7e84099a9e451
ItzArman <98105980+ItzArman@users.noreply.github.com>
2022-01-23 15:58:49 +0530

    Create 01_HelloWorld.py

---
+++ 01_HelloWorld.py
@@ -0,0 +1,11 @@
+#Welcome to the first chapter of python, "Hello World"
+ 
+ #importing modules
+ import os
+ 
+#lets start with printing some text
+print("Hello word") # first code of my life
+
+print("To comment, Just type # in the starting of line but for multiple lines you can apply ''' on the beginningbqnd ending of lines")
+
+# i got to learn about what is python, modules, pip, comments
